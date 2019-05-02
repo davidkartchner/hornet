1a2bc9b9357531d682712c13f8f99aeda8dcc3b4
davidkartchner <davidkartchner@gmail.com>
2019-05-02 15:05:29 -0400

    debug max clique

--- hornetsnest/src/Static/KCore/CoreNumbers.cu
+++ hornetsnest/src/Static/KCore/CoreNumbers.cu
@@ -197,7 +197,7 @@ struct GetLocalClique{
         int offset = vertex_nbhr_offsets[v_id];
         printf("Offset: ");
         printf(offset);
-        printf("\n")
+        printf("\n");
         for (vid_t i = 0; i < length_v; i++){
             vid_t u_id = vNeighPtr[i]; 
             // Vertex u = hornet.vertex(u_id); // How can I get this?
