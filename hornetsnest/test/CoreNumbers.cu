292820c6ae5b37b5737a2ff9b0d3cc22d02f9240
davidkartchner <davidkartchner@gmail.com>
2019-05-02 22:39:26 -0400

    debug max clique

--- hornetsnest/test/CoreNumbers.cu
+++ hornetsnest/test/CoreNumbers.cu
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     using namespace graph::structure_prop;
     using namespace graph::parsing_prop;
 
-    graph::GraphStd<vid_t, eoff_t> graph(UNDIRECTED);
+    graph::GraphStd<vid_t, eoff_t> graph(DIRECTED);
     graph.read(argv[1], SORT | PRINT_INFO);
 
     HornetInit hornet_init(graph.nV(), graph.nE(), graph.csr_out_offsets(),
